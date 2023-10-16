60d4c827f3a94c1c3dc0ebc4ff21185f2a57375c
Chugunovvvv <cugunovroman03@gmail.com>
2023-10-16 23:12:44 +0300

    fix bags 2

--- js/script.js
+++ js/script.js
@@ -56,17 +56,19 @@ function validatePassword() {
     }
     passwordError.innerHTML = '';
     inputPassword.style.border = '2px solid rgb(46,138,86)';
+    return true;
 }
 function validatePasswordA() {
     const passwordA = inputPasswordA.value;
     const passwordB = inputPassword.value;
-    if(passwordA !== passwordB || passwordA == 0) {
+    if(passwordA !== passwordB || passwordA.length == 0) {
         inputPasswordA.style.border = '2px solid red';
         passwordAError.innerHTML = 'Пароли не совпадают';
         return false;
     }
     passwordAError.innerHTML = '';
     inputPasswordA.style.border = '2px solid rgb(46,138,86)';
+    return true;
 }
 btnReset.addEventListener('click', () => {
     inputName.style.border = '';
@@ -80,10 +82,10 @@ btnReset.addEventListener('click', () => {
     submitError.innerHTML = ''
 })
 btnSubmit.addEventListener('click', () => {
-    if(!validateName() || !validateEmail() || validatePassword() || validatePasswordA()) {
-        submitError.style.display = 'block'
+    if(!validateName() || !validateEmail() || !validatePassword() || !validatePasswordA()) {
+        submitError.style.display = 'block';
         submitError.style.color = 'red';
-        submitError.innerHTML = 'Пожалуйста, исправьте ошибки';
+        submitError.innerHTML = 'Пожалуйста, исправьте ошибки!';
         setTimeout(function() {
             submitError.style.display = 'none';
         }, 4500)
@@ -91,9 +93,9 @@ btnSubmit.addEventListener('click', () => {
     }
     submitError.style.display = 'block'
     submitError.style.color = 'rgb(46,138,86)'
-    submitError.innerHTML = 'Данные успешно отправлены!';
+    submitError.innerHTML = 'Данные успешно отправлены!'
     setTimeout(function() {
         submitError.style.display = 'none';
     }, 5000)
-
+    return true;
 })
